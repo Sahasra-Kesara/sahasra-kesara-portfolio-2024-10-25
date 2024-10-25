ceaa003b4be963bd464dda447fc60933a6e3a0ee
Sahasra-Kesara <121740972+Sahasra-Kesara@users.noreply.github.com>
2024-10-26 04:24:48 +0530

    add gallery

--- src/components/MyImages.jsx
+++ src/components/MyImages.jsx
@@ -3,19 +3,8 @@ import { useRef } from "react";
 
 const MyImages = () => {
   return (
-    <div className="bg-neutral-800">
-      <div className="flex h-48 items-center justify-center">
-        <span className="font-semibold uppercase text-neutral-500">
-          Scroll down
-        </span>
-      </div>
+
       <HorizontalScrollCarousel />
-      <div className="flex h-48 items-center justify-center">
-        <span className="font-semibold uppercase text-neutral-500">
-          Scroll up
-        </span>
-      </div>
-    </div>
   );
 };
 
@@ -28,7 +17,7 @@ const HorizontalScrollCarousel = () => {
   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
 
   return (
-    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
+    <section ref={targetRef} className="relative h-[300vh]">
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
         <motion.div style={{ x }} className="flex gap-4">
           {cards.map((card) => {
@@ -44,7 +33,7 @@ const Card = ({ card }) => {
   return (
     <div
       key={card.id}
-      className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
+      className="group relative h-[450px] w-[450px]"
     >
       <div
         style={{
@@ -54,11 +43,6 @@ const Card = ({ card }) => {
         }}
         className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
       ></div>
-      <div className="absolute inset-0 z-10 grid place-content-center">
-        <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
-          {card.title}
-        </p>
-      </div>
     </div>
   );
 };
@@ -90,5 +74,10 @@ const cards = [
     url: "MyImages/5.jpg",
     title: "Title 5",
     id: 5,
+  },
+  {
+    url: "MyImages/6.jpg",
+    title: "Title 6",
+    id: 6,
   },
 ];
